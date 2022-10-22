223bdba89f89ba2d400fd0988096ab59d294351d
abdullahmusa <abdullahmusa02@gmail.com>
2022-10-22 19:10:51 +0200

    data classes

---
+++ app/src/main/java/hu/bme/aut/android/trackio/data/User.kt
@@ -0,0 +1,18 @@
+package hu.bme.aut.android.trackio.data
+
+import androidx.room.Entity
+
+@Entity
+data class User(
+    val id: Int,
+    val username: String,
+    val password: String,
+    val Authentication: String,
+    val emailAddress: String,
+
+    val height: Int,
+    val gender: String,
+    val birthDate: Long,
+    val goalSteps: Int,
+    val goalWeight: Int
+)
