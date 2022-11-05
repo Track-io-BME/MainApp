9d6648199abc29029a7987f4bd7ba84806dc08e4
abdullahmusa <abdullahmusa02@gmail.com>
2022-11-05 20:40:57 +0100

    WorkoutViewModel/Timer

---
+++ app/src/main/java/hu/bme/aut/android/trackio/model/WorkoutViewModel.kt
@@ -0,0 +1,49 @@
+package hu.bme.aut.android.trackio.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.*
+
+
+class WorkoutViewModel(application: Application) : AndroidViewModel(application) {
+    private lateinit var timer : Timer
+    private val _time = MutableLiveData(0)
+    val time : LiveData<Int> = _time
+    private var _timerRunning = MutableLiveData(false)
+    val timerRunning : LiveData<Boolean> = _timerRunning
+
+    fun startStop() {
+        if (_timerRunning.value == true) {
+            stopTimer()
+        }
+        else {
+            startTimer()
+        }
+    }
+
+    private fun startTimer() {
+        timer = Timer()
+        timer.scheduleAtFixedRate(
+            object : TimerTask() {
+                override fun run() {
+                    _time.postValue(_time.value?.plus(1))
+                }
+            },
+            0,
+            1000
+        )
+        _timerRunning.value = true
+    }
+
+    private fun stopTimer() {
+        timer.cancel()
+        timer.purge()
+        _timerRunning.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
--- app/src/main/java/hu/bme/aut/android/trackio/ui/duringworkout/DuringWorkoutFragment.kt
+++ app/src/main/java/hu/bme/aut/android/trackio/ui/duringworkout/DuringWorkoutFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import hu.bme.aut.android.trackio.R
 import hu.bme.aut.android.trackio.databinding.FragmentDuringWorkoutBinding
+import hu.bme.aut.android.trackio.model.WorkoutViewModel
 
 class DuringWorkoutFragment : Fragment() {
     private lateinit var binding : FragmentDuringWorkoutBinding
+    private val viewModel: WorkoutViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +21,17 @@ class DuringWorkoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDuringWorkoutBinding.inflate(inflater, container, false)
+//        viewModel = ViewModelProvider(requireActivity())[WorkoutViewModel::class.java]
+        viewModel.time.observe(viewLifecycleOwner) {
+            val seconds = it % 60
+            var minutes = it / 60
+            val hours = minutes / 60
+            minutes %= 60
+            binding.tvTimeValue.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        }
+        viewModel.timerRunning.observe(viewLifecycleOwner) {
+            binding.btnPlayPause.isActivated = it
+        }
         return binding.root
     }
 
@@ -28,10 +42,7 @@ class DuringWorkoutFragment : Fragment() {
             findNavController().navigate(R.id.action_duringWorkoutFragment_to_workoutMapFragment)
         }
         binding.btnPlayPause.setOnClickListener {
-            binding.btnPlayPause.isActivated = !binding.btnPlayPause.isActivated
+            viewModel.startStop()
         }
-//        binding.btnDuringToWorkoutMenu.setOnClickListener {
-//            findNavController().navigate(R.id.action_duringWorkoutFragment_to_workoutMenuFragment)
-//        }
     }
 }
